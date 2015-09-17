d6e98c0cb3cdb173b76ef1fc77c7698ea9c03ad8
ycymio <546183584@qq.com>
2015-09-17 10:47:06 +0800

    update pattern

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    echo "用户名：".$_POST['name']."<br>密码:".$_POST['password'];
+//	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+//		$uri = 'https://';
+//	} else {
+//		$uri = 'http://';
+//	}
+//	$uri .= $_SERVER['HTTP_HOST'];
+//	header('Location: '.$uri.'/dashboard/');
+//	exit;
+?>
+<!--Something is wrong with the XAMPP installation :-(-->
