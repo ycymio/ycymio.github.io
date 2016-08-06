180053a9e43ccb8ee9b0376b5915bb065e204f88
ycymio <546183584@qq.com>
2016-08-06 23:30:51 +0800

    add the bookcase.

---
+++ book/js/tj.js
@@ -0,0 +1,4 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb88cfc1ccab788f0903cac38c894caa3' type='text/javascript'%3E%3C/script%3E"));
+document.write('<scr'+'ipt src="http://s73.cnzz.com/stat.php?id=1585378&web_id=1585378" language="JavaScript"></sc'+'ript>');
+document.write('<sc'+'ript src="http://s96.cnzz.com/stat.php?id=4180674&web_id=4180674" language="JavaScript"></scr'+'ipt>');
---
+++ book/js/yxj.js
@@ -0,0 +1,3 @@
+/*ящй╬сроб╫г*/
+var cpro_id = "u671431";
+document.write('<scri'+'pt src="http://cpro.baidustatic.com/cpro/ui/f.js" type="text/javascript"></sc'+'ript>');
